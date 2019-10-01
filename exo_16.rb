efdfd1b0894cc79bd32ebd2e653b87c6b28220a0
fthiounn <fthiounn@gmail.com>
2019-10-01 09:31:02 +0200

    update projet 2

--- exo_16.rb
+++ exo_16.rb
@@ -5,16 +5,9 @@ birth_year = gets.to_i
 x=0
 
 while birth_year < current_year
- print "Il y a "
- print current_year - birth_year
- print " ans, tu avais "
- print x
- puts " ans"
+ puts "Il y a #{current_year-birth_year} ans, tu avais #{x} ans"
  x = x + 1
  birth_year = birth_year + 1
 end
 
-print "Cette année"
-print " tu as "
-print x
-puts " ans"
+puts "Cette année tu as #{x} ans !"
--- pyramide.rb
+++ pyramide.rb
@@ -4,9 +4,11 @@ print "> "
 user_input = gets.to_i
 output = "";
 puts "Voici la pyramide :"
+
 for a in 1..user_input do
 	output = output + " "
 end
+
 for a in 1..user_input do
 	output[user_input-a] = "#"
 	puts output
